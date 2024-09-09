690ef04054a24d6e16721959e0755e56c1b0fdfa
Zubaer-ahamad <zunaedshibli26@gmail.com>
2024-09-09 16:59:29 +0600

    tab responsive complate

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,37 +1,71 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     window.addEventListener('load', AOS.refresh)
     AOS.init();
 
     $('.destination-card').slick({
         centerMode: true,
-        centerPadding: '30px',
+        centerPadding: '20px',
         slidesToShow: 3,
         prevArrow: '.prev-btn',
         nextArrow: '.next-btn',
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 1440,
                 settings: {
-                    arrows: false,
+                    arrows: true,
                     centerMode: true,
-                    centerPadding: '25px',
+                    centerPadding: '40px',
                     slidesToShow: 3
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 1132,
                 settings: {
                     arrows: false,
                     centerMode: true,
-                    centerPadding: '20px',
+                    centerPadding: '50px',
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '30px',
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '100px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '10px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 320,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '5px',
                     slidesToShow: 1
                 }
             }
         ]
     });
-
-
 
 
 })
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 document.getElementById("home-link").classList.add("active");
-// document.getElementById("home-link-1").classList.add("active");
 const links = document.querySelectorAll(".nav-icon");
 
 links.forEach(link => {
@@ -7,4 +6,11 @@ links.forEach(link => {
         links.forEach(link => link.classList.remove("active"));
         link.classList.add("active");
     });
+});
+
+
+const menuBtn = document.getElementById("menu-btn");
+menuBtn.addEventListener("click", () => {
+    const navIcon = document.getElementById("nav-icon-div");
+    navIcon.classList.toggle("active-2");
 });
